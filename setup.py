43d89ce1109e8b1fa2eeb38bfb058e0533400480
Santhosh <dev.santhoshp@gmail.com>
2024-05-02 19:34:42 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import find_packages, setup
+
+VERSION = '5.0'
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-admin-extended',
+    version=VERSION,
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Enhance UI/UX of django admin',
+    long_description=README,
+    url='https://github.com/santhosh-programmer/django-admin-extended',
+    author='Cuong Nguyen',
+    author_email='dev.santhoshp@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 4.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
